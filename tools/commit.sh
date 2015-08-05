528d8f7aebcd16506aea55ef638ec08b410a76ea
wangjiezhe <wangjiezhe@gmail.com>
2015-08-05 09:45:42 +0800

    Remove output of curl

--- tools/commit.sh
+++ tools/commit.sh
@@ -10,7 +10,7 @@ source functions.sh
 class_number=$1
 
 if is_number ${class_number}; then
-	class_name=$(curl "https://www.shiyanlou.com/courses/${class_number}" | grep "<title>" | sed "s/^[[:space:]]*//g" | sed "s@<title>\(.*\) - \(.*\)</title>@\1@")
+	class_name=$(curl -s "https://www.shiyanlou.com/courses/${class_number}" | grep "<title>" | sed "s/^[[:space:]]*//g" | sed "s@<title>\(.*\) - \(.*\)</title>@\1@")
 	commit_message="Add code for course ${class_number}: ${class_name}"
 	# echo "${class_number}"
 	# echo "${class_name}"
